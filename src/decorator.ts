de913f42fc641b453ac8f4c74d9173aa536a91d5
YiChun Sung <ycsung.r@gmail.com>
2020-10-30 02:58:22 +0800

    Added createMappingDecorator

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-
+/*
 function classDecorator() {
 	return (target) => {
 		Reflect.defineMetadata('classMetaData', 'a', target);
@@ -22,15 +22,63 @@ class ClassName {
 let data = Reflect.getMetadata('classMetaData', ClassName);
 
 let data2 = Reflect.getMetadata('methodMetaData', new ClassName(), 'someMethod');
+*/
+// console.log(data);
+// console.log(data2);
 
-console.log(data);
-console.log(data2);
+const PATH_METADATA = 'path';
+const METHOD_METADATA = 'method';
 
-const METHOD_METADATA = 'method'；
-const PATH_METADATA = 'path'；
-
-function controller(path: string) {
+function Controller(path: string) {
 	return function(target) {
 		Reflect.defineMetadata(PATH_METADATA, path, target);
 	}
 }
+
+function createMappingDecorator(method: string) {
+	return function(path: string) {
+		return (target, key, descriptor) => {
+	    Reflect.defineMetadata(PATH_METADATA, path, descriptor.value);
+	    Reflect.defineMetadata(METHOD_METADATA, method, descriptor.value);
+	  }
+	}
+}
+
+const Get = createMappingDecorator('GET');
+const Post = createMappingDecorator('POST');
+
+@Controller('/heloo')
+class PlayData {
+  @Get('/try')
+  someGetMethod() {
+    return 'hello world';
+  }
+
+  @Post('/yoyo')
+  somePostMethod() {}
+}
+
+function mapRoute(instance: Object) {
+  const prototype = Object.getPrototypeOf(instance);
+
+  const methodsNames = Object.getOwnPropertyNames(prototype).filter(item => item);
+  return methodsNames.map(methodName => {
+    const fn = prototype[methodName];
+
+    const route = Reflect.getMetadata(PATH_METADATA, fn);
+    const method = Reflect.getMetadata(METHOD_METADATA, fn);
+    return {
+      route,
+      method,
+      fn,
+      methodName
+    }
+  })
+};
+
+let he = Reflect.getMetadata(PATH_METADATA, PlayData); // '/test'
+
+console.log(he);
+let a = mapRoute(new PlayData());
+
+console.log(a);
